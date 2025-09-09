dcbfeac11b46a6d38045890ca2b262dc496c5e25
PuneetVerma07 <pk2382849@gmail.com>
2025-09-09 06:17:39 +0530

    feat: implement user form and display functionality

--- js_part3/script.js
+++ js_part3/script.js
@@ -1,37 +1,68 @@
-//global
-console.log(this)
+let form = document.querySelector("form");
+let username = document.querySelector("#name");
+let role = document.querySelector("#role");
+let bio = document.querySelector("#bio");
+let photo = document.querySelector("#photo");
 
-//function k ander
-function abcd() {
-  console.log(this)
-}
-abcd();
+const userManager = {
+  users: [],
+  init: function () {
+    form.addEventListener("submit", this.submitForm.bind(this));
+  },
+  submitForm: function (e) {
+    e.preventDefault();
+    this.addUser();
+  },
+  addUser: function () {
+    this.users.push({
+      username: username.value,
+      role: role.value,
+      bio: bio.value,
+      photo: photo.value,
+    });
 
-//method k ander
-let obj = {
-  name: "Puneet",
-  age: 23,
-  sayAge: function () {
-    let defg = () => {
-      console.log(this)
-    }
-    defg();
-  }
-}
-obj.sayAge();
+    form.reset();
+    this.renderUi();
+  },
+  renderUi: function () {
+    document.querySelector(".users").innerHTML = "";
+    this.users.forEach(function (user) {
+      const card = document.createElement("div");
+      card.className =
+        "bg-white/90 background-blur rounded-2xl shadow-xl p-8 flex flex-col items-center border border-blue-100 hover:scale-105 transition";
 
-//eventhendler k ander
-document.querySelector("h1").addEventListener("click", function () {
-  console.log(this.style.color = "red")
-})
+      //image
+      const img = document.createElement("img");
+      img.className =
+        "w-28 h-28 rounded-full object-cover mb-5 border-4 border-blue-200 shadow";
+      img.src = user.photo;
+      img.alt = "User photo";
+      card.appendChild(img);
 
-//class k ander
-class Abcd{
-  constructor() {
-    console.log("hey")
-    this.a = 12;
-  }
-}
+      //Name
+      const name = document.createElement("h2");
+      name.className = "text-2xl font-bold mb-1 text-blue-700";
+      name.textContent = user.username;
+      card.appendChild(name);
 
-let val = new Abcd();
-console.log(val)
+      //Role
+      const role = document.createElement("p");
+      role.className = "text-purple-500 mb-2 font-medium";
+      role.textContent = user.role;
+      card.appendChild(role);
+
+      //Description
+      const desc = document.createElement("p")
+      desc.className = "text-gray-700 text-center"
+      desc.textContent = user.bio
+      card.appendChild(desc)
+
+      //finally append the card whenever needed, for example:
+
+      document.querySelector(".users").appendChild(card)
+    });
+  },
+  removeUser: function () {},
+};
+
+userManager.init();
